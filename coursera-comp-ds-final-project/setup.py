bd88716550f0dd531e6143e8d711c8964096e682
mortennp <mortennp@hotmail.com>
2019-01-12 16:08:49 +0100

    comp ds baseline

---
+++ coursera-comp-ds-final-project/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Coursera Competetive Data Science Final Project',
+    author='mortennp',
+    license='BSD-3',
+)
