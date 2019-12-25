91ef060edbc3d3343a8243279bd098d27ef43d6e
mortennp <mortennp@hotmail.com>
2019-12-25 15:45:40 +0100

    Add she-bank go run-cont

--- SmillaAI/motioneye/run-container.sh
+++ SmillaAI/motioneye/run-container.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 docker run --name="motioneye" -p 8765:8765 --hostname="motioneye" \
     -v /etc/localtime:/etc/localtime:ro \
     -v /opt/motioneye/conf:/etc/motioneye \
     -v /opt/motioneye/data:/var/lib/motioneye \
     --device=/dev/video0 \
     --restart="always" \
-    --detach=true \ 
+    --detach=true \
     ccrisan/motioneye:master-armhf
