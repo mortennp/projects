77748d53be0526331f2c388c3cdc76a4313cda18
mortennp <mortennp@hotmail.com>
2019-12-29 10:44:02 +0100

    Add time to SenseLED outptu

--- SmillaAI/wiring/SenseLED.c
+++ SmillaAI/wiring/SenseLED.c
@@ -4,28 +4,42 @@
 * Author      : freenove
 * modification: 2016/06/12
 **********************************************************************/
-#include <wiringPi.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <wiringPi.h>
 
 #define ledPin    1  	//define the ledPin
 #define sensorPin 0		//define the sensorPin
 
+char* getTimeString(void)
+{
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	time( &rawtime );
+	timeinfo = localtime( &rawtime );
+	return asctime( timeinfo );
+}
+
 int main(void)
 {
+	printf("Starting...");
+
 	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
-		printf("setup wiringPi failed !");
+		printf("Setup of wiringPi failed!");
 		return 1; 
 	}
 	
 	pinMode(ledPin, OUTPUT); 
 	pinMode(sensorPin, INPUT);
-	int i = 0;
+	int i = 0;	
 	int status = LOW;
 	while(1){		
-		if(digitalRead(sensorPin) == HIGH){ //if read sensor for high level
+		if (digitalRead(sensorPin) == HIGH){ //if read sensor for high level
 			if (HIGH != status) {
 				digitalWrite(ledPin, HIGH);   //led on
-				printf("%i: led on...\n", i++);
+				printf("%s %i: led on...\n", getTimeString(), i++);
 				status = HIGH;
 			}
 		}
@@ -33,7 +47,7 @@ int main(void)
 			if (LOW != status)
 			{
 				digitalWrite(ledPin, LOW);   //led off
-				printf("%i: ...led off\n", i++);
+				printf("%s %i: ...led off\n", getTimeString(), i++);
 				status = LOW;
 			}
 		}
---
+++ SmillaAI/wiring/run-wiring.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+build/SenseLED | tee -a SenseLED_out.txt
