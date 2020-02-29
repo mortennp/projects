2a75fb5158cba86ddaa7d8eb97c9eb2b171c3969
mortennp <mortennp@hotmail.com>
2020-02-29 23:03:39 +0100

    mounting usb to plex on container restart

--- plex/mount.sh
+++ plex/mount.sh
@@ -3,3 +3,4 @@ mkdir -p /mnt/usb1/FLAC
 mount /dev/sda1 /mnt/usb1/FLAC
 mkdir -p /mnt/usb2/FLAC
 mount /dev/sdb1 /mnt/usb2/FLAC
+sh start.sh
--- plex/reset-run-container.sh
+++ plex/reset-run-container.sh
@@ -3,7 +3,7 @@ docker stop plex
 docker rm plex
 #rm -rf /opt/plex/config
 #rm -rf /opt/plex/transcode
-#docker build --tag plex https://github.com/greensheep/plex-server-docker-rpi.git
+#docker build --tag plex .
 docker run \
     -d \
     --name plex \
@@ -16,17 +16,7 @@ docker run \
     --device=/dev/sda1 \
     --device=/dev/sdb1 \
     --privileged \
-    -v $(pwd):/scripts \
-    plex \
-#    /bin/bash -c '/scripts/mount.sh'
-#    -v /dev/sda1:/mnt/usb1/FLAC \
-#    -v /dev/sdb1:/mnt/usb2/FLAC \
-docker exec plex mkdir -p /mnt/usb1/FLAC
-docker exec plex mount /dev/sda1 /mnt/usb1/FLAC
-docker exec plex ls /mnt/usb1/FLAC
-docker exec plex mkdir -p /mnt/usb2/FLAC
-docker exec plex mount /dev/sdb1 /mnt/usb2/FLAC
-docker exec plex ls /mnt/usb2/FLAC
+    plex 
 
 # !!!!!!!!!! IMPORTANT !!!!!!!!!!
 # Now VNC into RPi and use Chromium to sign-in to localhost:32400 using credentials from plex.tv!
